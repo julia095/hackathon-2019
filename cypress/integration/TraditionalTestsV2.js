913dfd109d419d5f097ef2ae7a344430210a9978
yulia_tekin <yulia@spotlightreporting.com>
2019-11-27 20:50:37 +1300

    readme and extra empty tests

--- cypress/integration/TraditionalTestsV2.js
+++ cypress/integration/TraditionalTestsV2.js
@@ -19,7 +19,7 @@ describe('Login page UI elements test', function() {
       .should('have.attr', 'href', 'index.html');
   });
 
-  it('Should display a header image on the right place correctly', function() {
+  it('Should display a header image correctly', function() {
     // Should be on the right place - not possible
     // Should look correctly - not possible
   });
@@ -34,19 +34,37 @@ describe('Login page UI elements test', function() {
     // Should look correctly - not possible
   });
 
-  /*Afterwards I put comments for each visual check which is not possible to
-    implement instead of creating a separate test.*/
-
   it('Should display UserName input correctly', function() {
     //Should be on the right place and look right - not possible
-    //Username title is visible and contains username
+  });
+
+  it('Should Username title be visible and contain username', function() {
     cy.get(gs.titleUsernameInput)
       .should('be.visible')
       .and('contain', 'Username');
-    //Username image is visible
+  });
+
+  it('Should Username title be visible and contain username', function() {
+    cy.get(gs.titleUsernameInput)
+      .should('be.visible')
+      .and('contain', 'Username');
+  });
+
+  it('Should Username image be visible', function() {
     cy.get(gs.imgUserName).should('be.visible');
-    //Username image should look correct - not possible
-    //Username input is on the place and it displays text hint
+  });
+  
+  it('Should display Username image correctly', function() {
+    // Should be on the right place - not possible
+    // Should look correctly - not possible
+  });
+  
+  it('Should Username input be displayed correctly', function() {
+    // Should be on the right place - not possible
+    // Should look correctly - not possible
+  });
+
+  it('Should Username input display text hint', function() {
     cy.get(gs.inputUserName)
       .should('be.visible')
       .and('have.attr', 'placeholder', 'Enter your username');
@@ -54,47 +72,68 @@ describe('Login page UI elements test', function() {
 
   it('Should display Password input correctly', function() {
     //Should be on the right place and look right - not possible
-    //Password title is visible and contains correct text
+  });
+
+  it('Should password title be visible and contain correct text', function() {
     cy.get(gs.titlePasswordInput)
       .should('be.visible')
       .and('contain', 'Password');
-    //Password image is visible
+  })    
+  
+  it('Should password image be visible', function() {
     cy.get(gs.imgPassword).should('be.visible');
-    //Password image should look correct - not possible
-    //Password input is on the place and it displays text hint
+  })
+    
+  it('Should display password image correctly ', function() {
+    //Should be on the right place and look right - not possible
+  })  
+    
+  it('Should display Password input correctly ', function() {
+    //Should be on the right place and look right - not possible
+  })
+
+  it('Should display Password input correctly ', function() {
+    //Should be on the right place and look right - not possible
+  })
+
+  it('Should Password input display text hint ', function() {
     cy.get(gs.inputPassword)
       .should('be.visible')
       .and('have.attr', 'placeholder', 'Enter your password');
-  });
+  })
+    
 
   it('Should display Sign in button correctly', function() {
     //Should be on the right place and look right - not possible
-    //Contains Sign in text
     //Background is blue - can't actually check ,'btn.btn-primary' - can change the color of the button
+  });
+
+  it('Should display Sign in button contain correct text', function() {
     cy.get(gs.btnSignIn).should('contain', 'Log In');
   });
 
   it('Should display Remember me checkbox correctly', function() {
     //Should be on the right place and look right - not possible
-    //Checkbox is visible
+  });
+
+  it('Should display Remember me checkbox be visible and have correct text close to it', function() {
     cy.get(gs.chbxRememberme)
       .find('input')
       .should('have.class', 'form-check-input')
       .and('be.visible');
-    //It has Remember me text close to it
     cy.get(gs.chbxRememberme)
       .find('label')
       .should('contain', 'Remember Me');
   });
 
-  it('Should display 3 images under sign in button ', function() {
+  it('Should 3 images under sign in button look correctly', function() {
     //Should be on the right place and look right - not possible to ensure without using additional page diff tools
-    // And even in this case so many times will be wrong results
-    // Should be visible
+  })
+
+  it('Should 3 images under sign in button be visible ', function() {
     cy.get(gs.imgTwitter).should('be.visible');
     cy.get(gs.imgFacebook).should('be.visible');
     cy.get(gs.imgLinkedin).should('be.visible');
-    // Should be correct image - not possible
   });
 });
 
@@ -120,7 +159,7 @@ describe('Data driven tests', function() {
   });
 });
 
-describe.only('Table sort test', function() {
+describe('Table sort test', function() {
   before(function() {
     cy.login(urlV2);
   });
@@ -146,7 +185,7 @@ describe('Canvas chart test', function() {
     // not possible as a bar chart is represented as a single canvas. I'm not able to reach graphs
   });
   it('Should add the data for the year 2019', function() {
-    // again not possible as a bar chart is represented as a single canvas. I'm not able to reach graphs
+    // not possible as a bar chart is represented as a single canvas. I'm not able to reach graphs
   });
 });
 
